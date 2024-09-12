99e666796e2157a9b93d338241a3f414561210a1
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-09-12 21:27:55 +0900

    Customize error page message with redirect
    Customize the error page message with a redirect that points to the ScalarDB docs site.

--- src/theme/NotFound/Content/index.tsx
+++ src/theme/NotFound/Content/index.tsx
@@ -3,8 +3,14 @@ import clsx from 'clsx';
 import Translate from '@docusaurus/Translate';
 import type {Props} from '@theme/NotFound/Content';
 import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
 
 export default function NotFoundContent({className}: Props): JSX.Element {
+  // Automatically redirect visitors to the ScalarDB docs site after a 10-second wait.
+  setTimeout(function() {
+    window.location.replace('https://scalardb.scalar-labs.com/docs/latest/');
+  }, 10000);
+
   return (
     <main className={clsx('container margin-vert--xl', className)}>
       <div className="row">
@@ -13,22 +19,25 @@ export default function NotFoundContent({className}: Props): JSX.Element {
             <Translate
               id="theme.NotFound.title"
               description="The title of the 404 page">
-              Page Not Found
+              The contents of this site have moved.
             </Translate>
           </Heading>
           <p>
             <Translate
               id="theme.NotFound.p1"
               description="The first paragraph of the 404 page">
-              We could not find what you were looking for.
+              The ScalarDB Community docs site has been merged with the&nbsp;
             </Translate>
+            <Link
+              to="https://scalardb.scalar-labs.com/docs/latest/">
+                ScalarDB docs site
+            </Link>.
           </p>
           <p>
             <Translate
               id="theme.NotFound.p2"
               description="The 2nd paragraph of the 404 page">
-              Please contact the owner of the site that linked you to the
-              original URL and let them know their link is broken.
+              You will be redirected shortly.
             </Translate>
           </p>
         </div>
